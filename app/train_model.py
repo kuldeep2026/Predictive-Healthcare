570d6fd3d2de5a64b044d0162bccff070ce1c7fe
Mohit Dhanotiya <mohit.d.2807@gmail.com>
2025-11-02 16:08:39 +0530

    Added code for displaying cross validated scores and accuracy

--- app/train_model.py
+++ app/train_model.py
@@ -13,16 +13,19 @@ X = mlb.fit_transform(df['Symptoms'])
 y = df['Disease']
 
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,cross_val_score
 import joblib 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = MultinomialNB()
+scores = cross_val_score(model, X_train, y_train, cv=5)
 model.fit(X_train, y_train)
 
 accuracy = model.score(X_test, y_test)
 print("Model Accuracy:", accuracy)
+print("Cross-Validation Scores:", scores)
+print("Cross-Validation Accuracy:", scores.mean())
 
 joblib.dump(model, "models/disease_model.joblib")
 joblib.dump(mlb, "models/symptom_encoder.joblib")
